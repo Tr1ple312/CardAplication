c1cbdb388e08a4d47db39ef2dbae9f4094f6879f
triple <wog20051221@gmail.com>
2026-01-17 19:29:06 +0100

    front: Card Component: +TextField

--- ReactFront/card-frontend/src/Components/MainPage.jsx
+++ ReactFront/card-frontend/src/Components/MainPage.jsx
@@ -4,47 +4,76 @@ import {
   CardActions,
   Typography,
   Button,
-} from '@mui/material';
+  TextField,
+  Input,
+} from "@mui/material";
+import { useState } from "react";
 
-export default function WordCard({ word, meaning }) {
-  return (
-    <Card
-      sx={{
-        width: { xs: '90%', sm: 800 },
-        maxWidth: 1000,
-        minHeight: 650,
-        padding: 4,
-        borderRadius: 8,
-        boxShadow: 6,
-        bgcolor: '#323261',
-        color: '#f8f8f2',
-        border: '20px solid #595d78',
-        display: 'flex',
-        flexDirection: 'column',
-      }}
-    >
-      <CardContent
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100%',
-          textAlign: 'center',
-        }}
-      >
-        <Typography variant="h1">{word}</Typography>
-        <Typography variant="h1">{meaning}</Typography>
-      </CardContent>
+export default function WordCard({ meaning, word }) {
+	const [userAnswer, setUserAnswer] = useState("");
+	const [isChecked, setIsChecked] = useState(false)
 
-      <CardActions sx={{ justifyContent: 'center' }}>
-        <Button size="large" sx={{ bgcolor: '#2f2f2f', color: 'primary.main' }}>
-          Learn more
-        </Button>
-      </CardActions>
-    </Card>
+	const normalUserAnswer = userAnswer.trim().toLowerCase()
+	const normalCorrectAnswer = word.trim().toLowerCase()
+	const isCorrect = normalUserAnswer === normalCorrectAnswer
 
+	function handleUserAnswer(e) {
+		setUserAnswer(e.target.value)
+		setIsChecked(false)
+	}
+
+	function handleKeyDown(e) {
+		if (e.key === 'Enter' && !isChecked && userAnswer.trim() !== '') {
+			setIsChecked(true)
+		}
+	}
+
+
+	return (
+		<Card
+			sx={{
+				width: { xs: "90%", sm: 800 },
+				maxWidth: 1000,
+				minHeight: 650,
+				padding: 4,
+				borderRadius: 8,
+				boxShadow: 6,
+				bgcolor:"#323261",
+				color: "#f8f8f2",
+				border:isChecked ? (isCorrect ? "20px solid #6bcf92" : "20px solid #e06a6a" ) : "20px solid #595d78",
+				display: "flex",
+				flexDirection: "column",
+			}}
+		>
+			<CardContent
+				sx={{
+					flexGrow: 1,
+					display: "flex",
+					flexDirection: "column",
+					justifyContent: "center",
+					alignItems: "center",
+					height: "100%",
+					textAlign: "center",
+				}}
+			>
+				<Typography variant="h1">{meaning}</Typography>
+				<TextField
+					variant="outlined"
+					label="Enter translation"
+					value={userAnswer}
+					onChange={handleUserAnswer}
+					onKeyDown={handleKeyDown}
+					autoFocus
+					InputProps= {{readOnly: isChecked}}
+					sx={{ width: '100%', maxWidth: 300 }}
+
+				/>
+			</CardContent>
+			<CardActions sx={{ justifyContent: "center" }}>
+				<Button size="large" sx={{ bgcolor: "#2f2f2f", color: "primary.main" }}>
+					Learn more
+				</Button>
+			</CardActions>
+		</Card>
   );
 }
-
-
