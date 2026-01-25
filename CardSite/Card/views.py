29e2e62921141f3ecf393f0b7bcf30b66d8250e8
triple <wog20051221@gmail.com>
2026-01-25 22:23:51 +0100

    Back: views.py + CardAPIDetail + patch, delete, get_object methods

--- CardSite/Card/views.py
+++ CardSite/Card/views.py
@@ -1,3 +1,5 @@
+from django.core.serializers import serialize
+from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -22,3 +24,33 @@ class CardAPIView(APIView):
             return Response({'card': serializer.data}, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CardAPIDetail(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk):
+        try:
+            return Card.objects.get(pk=pk, user=self.request.user)
+        except Card.DoesNotExist:
+            raise NotFound('Card not found')
+
+    def get(self, request, pk):
+        card = self.get_object(pk)
+        serializer = CardSerializer(card)
+        return Response(serializer.data)
+
+    def patch(self, request, pk):
+        card = self.get_object(pk)
+        serializer = CardSerializer(card, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request, pk):
+        card = self.get_object(pk)
+        card.delete()
+        return Response(status=204)
--- CardSite/CardSite/urls.py
+++ CardSite/CardSite/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from Card.views import CardAPIView
+from Card.views import CardAPIView, CardAPIDetail
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/cards/', CardAPIView.as_view()),
+    path('api/v1/cards/<int:pk>/', CardAPIDetail.as_view())
     ]
